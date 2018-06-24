34e7619ed5690ae784deb53167fe9e7d4bf73be0
shibe23 <shibe23.info@gmail.com>
2018-06-25 00:13:19 +0900

    delete comment

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -3,7 +3,6 @@ namespace tutorial_dotnet_core_mvc.Controllers
 {
     public class HelloController : Controller
     {
-        //IActionResultオブジェクトはContollerクラスで用意されたメソッドで制定できる
         public IActionResult Index()
         {
             return Content("Hello!!");
