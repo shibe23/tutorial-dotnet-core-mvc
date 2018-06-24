dc215b5a18d393bbf8cb27c37c05bf5f605353fe
shibe23 <shibe23.info@gmail.com>
2018-06-25 00:02:48 +0900

    add HelloController

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace tutorial_dotnet_core_mvc.Controllers
+{
+    public class HelloController : Controller
+    {
+        //IActionResultオブジェクトはContollerクラスで用意されたメソッドで制定できる
+        public IActionResult Index()
+        {
+            return Content("Hello!!");
+        }
+    }
+}
