8fa3f597a9485c7739dcfa9aea328b05fddb8588
shibe23 <shibe23.info@gmail.com>
2018-06-27 00:42:42 +0900

    add Model Class

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+namespace tutorial_dotnet_core_mvc.Controllers
+{
+  public class Book
+  {
+    public int Id {get; set;}
+    public string Title {get; set;}
+    public int Price {get; set;}
+    public string Publisher {get; set;}
+    public bool Sample {get; set;}
+  }
+}
