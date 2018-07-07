7ec7b8166f229c8221e38b7703749437b1b2ac98
shibe23 <shibe23.info@gmail.com>
2018-07-07 23:43:24 +0900

    Add DisplayName

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel;
 namespace tutorial_dotnet_core_mvc.Models
 {
   public class Book
   {
     public int Id {get; set;}
+    [DisplayName("書名")]
     public string Title {get; set;}
+    [DisplayName("価格")]
     public int Price {get; set;}
+    [DisplayName("出版社")]
     public string Publisher {get; set;}
+    [DisplayName("配布サンプル")]
     public bool Sample {get; set;}
   }
 }
